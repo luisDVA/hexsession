a9171fb21565141a3e6802b1e402fca00f34692d
LuisDVA <luisd@ciencias.unam.mx>
2024-10-07 12:33:26 -0600

    fix for pkgs without explicit urls

--- R/helpers.R
+++ R/helpers.R
@@ -1,9 +1,11 @@
 #' Get loaded packages
 #' @return A character vector of the attached packages (excludes base packages)
 getLoaded <- function() {
-  #also exclude hexsession
-  basepkgs <- c("stats", "graphics", "grDevices", "utils", "datasets",
-                "methods", "base","hexsession")
+  # also exclude hexsession
+  basepkgs <- c(
+    "stats", "graphics", "grDevices", "utils", "datasets",
+    "methods", "base", "hexsession"
+  )
   (.packages())[!(.packages()) %in% basepkgs]
 }
 
@@ -13,9 +15,10 @@ getLoaded <- function() {
 find_imgpaths <- function(pkgnames) {
   lapply(pkgnames, function(x) {
     list.files(system.file(package = x),
-               pattern = "\\.png$|\\.jpg$|\\.svg$",
-               recursive = TRUE,
-               full.names = TRUE)
+      pattern = "\\.png$|\\.jpg$|\\.svg$",
+      recursive = TRUE,
+      full.names = TRUE
+    )
   })
 }
 
@@ -25,7 +28,7 @@ find_imgpaths <- function(pkgnames) {
 find_logopaths <- function(imagepaths) {
   logopaths <- lapply(imagepaths, function(x) x[grepl("logo", x, ignore.case = TRUE)])
   logopaths[lengths(logopaths) == 0] <- NA_character_
-  logopaths <- sapply(logopaths,"[[",1)
+  logopaths <- sapply(logopaths, "[[", 1)
   unlist(logopaths)
 }
 
@@ -34,10 +37,18 @@ find_logopaths <- function(imagepaths) {
 #' @importFrom utils packageDescription
 #' @return A vector of package URLs
 pkgurls <- function(pkgnames) {
-  allurls <- purrr::map(pkgnames,\(x) packageDescription(x,fields="URL"))
-  splturls <- purrr::map(allurls,\(x) unlist(strsplit(x,",|\n")))
-  purrr::map_chr(splturls,`[`,1)
+  allurls <- purrr::map(pkgnames, \(x) {
+    url <- packageDescription(x, fields = "URL")
+    if (is.null(url) || all(is.na(url)) || all(url == "")) NA_character_ else url
+  })
+  splturls <- purrr::map(allurls, \(x) {
+    if (all(is.na(x))) NA_character_ else unlist(strsplit(x, ",|\n"))
+  })
+  purrr::map_chr(splturls, \(x) {
+    if (all(is.na(x))) NA_character_ else x[1]
+  })
 }
+
 
 
 #' Encode image to Base64
@@ -45,14 +56,17 @@ pkgurls <- function(pkgnames) {
 #' @return Base64 encoded string of the image
 #' @export
 encode_image <- function(file_path) {
-  tryCatch({
-    encoded <- base64enc::base64encode(file_path)
-    ext <- tolower(tools::file_ext(file_path))
-    paste0("data:image/", ext, ";base64,", encoded)
-  }, error = function(e) {
-    warning(paste("Error encoding file:", file_path, "-", e$message))
-    NULL
-  })
+  tryCatch(
+    {
+      encoded <- base64enc::base64encode(file_path)
+      ext <- tolower(tools::file_ext(file_path))
+      paste0("data:image/", ext, ";base64,", encoded)
+    },
+    error = function(e) {
+      warning(paste("Error encoding file:", file_path, "-", e$message))
+      NULL
+    }
+  )
 }
 
 #' Create missing logos
@@ -65,20 +79,19 @@ make_missingLogos <- function(attached_pkgs, logopaths) {
 
 
   create_logo <- function(pkgname) {
-
     widthhex <- magick::image_info(basehex)$width
 
     fig <- magick::image_blank(700, 700) |>
-      magick::image_annotate(pkgname, size = 100, color = 'white') |>
+      magick::image_annotate(pkgname, size = 100, color = "white") |>
       magick::image_trim() |>
-      magick::image_border('none') |>
-      magick::image_resize(paste0(widthhex-30, 'x'))
+      magick::image_border("none") |>
+      magick::image_resize(paste0(widthhex - 30, "x"))
 
-    magick::image_composite(basehex, fig, operator = 'SrcOver',gravity = "center")
+    magick::image_composite(basehex, fig, operator = "SrcOver", gravity = "center")
   }
 
-  logoimgs <- purrr::map(pending,create_logo)
-  imgpaths <- paste0(tempfile(),"_",pending,".png")
-  purrr::walk2(logoimgs,imgpaths,\(x,y) magick::image_write(x,y))
+  logoimgs <- purrr::map(pending, create_logo)
+  imgpaths <- paste0(tempfile(), "_", pending, ".png")
+  purrr::walk2(logoimgs, imgpaths, \(x, y) magick::image_write(x, y))
   imgpaths
 }
